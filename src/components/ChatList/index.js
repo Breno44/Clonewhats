f99a6d5080c171eb1e46ee3eb08e2464022313bc
Breno Henrique <bhfs.2908@gmail.com>
2021-08-14 12:20:12 -0300

    feat:include chat

--- src/components/ChatList/index.js
+++ src/components/ChatList/index.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import './styles.css';
 
-export default () => {
+export default ({onClick, active, data}) => {
     return (
-        <div className="chatListItem">
-            <img className="chatListItem--avatar" src="https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt="" />
+        <div 
+         className={`chatListItem ${active ? 'active' : ''}`}
+         onClick={onClick}
+        >
+            <img className="chatListItem--avatar" src={data.image} alt="" />
             <div className="chatListItem--lines">
                 <div className="chatListItem--line">
-                    <div className="chatListItem--name">Breno Henrique</div>
+                    <div className="chatListItem--name">{data.title}</div>
                     <div className="chatListItem--date">19:00</div>
                 </div>
                 <div className="chatListItem--line">
