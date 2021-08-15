701eba78b1c2251e418fa8d822faf603a3bd8144
Breno Henrique <bhfs.2908@gmail.com>
2021-08-15 12:10:01 -0300

    feat:message

---
+++ src/components/MessageItem/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './styles.css';
+
+export default ({data, user}) => {
+    return(
+        <div 
+            className="messageLine"
+            style={{
+                justifyContent: user.id === data.author ? 'flex-end' : 'flex-start',
+            }}
+        >
+            <div 
+                className="messageItem"
+                style={{
+                    backgroundColor: user.id === data.author ? '#dcf8c6' : '#fff',
+                }}
+            >
+                <div className="messageText">{data.body}</div>
+                <div className="messageDate">19:00</div>
+            </div>
+        </div>
+    );
+}
