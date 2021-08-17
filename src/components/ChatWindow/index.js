9bdb9ed8037375192d748891ea2d054cad916cdb
Breno Henrique <bhfs.2908@gmail.com>
2021-08-17 17:17:32 -0300

    feat:finishing

--- src/components/ChatWindow/index.js
+++ src/components/ChatWindow/index.js
@@ -4,8 +4,9 @@ import EmojiPicker from 'emoji-picker-react';
 import "./styles.css";
 import MessageItem from '../MessageItem/index'
 import { Search, AttachFile, MoreVert, InsertEmoticon, Close, Send, Mic } from '@material-ui/icons';
+import Api from '../../api'
 
-export default ({user}) => {
+export default ({user, data}) => {
 
     const body = useRef();
 
@@ -18,44 +19,21 @@ export default ({user}) => {
     const [emojiOpen, setEmojiOpen] = useState(false);
     const [text, setText] = useState('');
     const [listening, setListening] = useState(false);
-    const [list, setList] = useState([
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-        {author: 123, body: 'Oii meu nome e Breno'},
-        {author: 123, body: 'td bem com vc?'},
-        {author: 1234, body: 'Testando internet'},
-    ]);
+    const [list, setList] = useState([]);
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         if(body.current.scrollHeight > body.current.offsetHeight) {
             body.current.scrollTop = body.current.scrollHeight - body.current.offsetHeight;
         }
     }, [list])
+
+    useEffect(() => {
+        setList([]);
+
+        let unsub = Api.onChatContent(data.chatId, setList, setUsers);
+        return unsub;
+    }, [data.chatId])
 
     const handleEmojiClick = (e, emojiObject) => {
         setText(text + emojiObject.emoji)
@@ -87,16 +65,26 @@ export default ({user}) => {
         }
     }
 
-    const handleSendClick = () => {
+    const handleInputKeyUp = (e) => {
+        if(e.keyCode === 13) {
+            handleSendClick();
+        }
+    }
 
+    const handleSendClick = () => {
+        if(text.length > 0) {
+            Api.sendMessage(data, user.id, 'text', text, users)
+             setText('')
+             setEmojiOpen(false);
+        }
     }
 
     return (
         <div className="chatWindow">
             <div className="chatWindow--header">
                 <div className="chatWindow--headerinfo">
-                    <img className="chatWindow--avatar" src="https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt="" />
-                    <div className="chatWindow--name">Fulano de tal</div>
+                    <img className="chatWindow--avatar" src={data.image} alt="" />
+                    <div className="chatWindow--name">{data.title}</div>
                 </div>
                 <div className="chatWindow--headerbuttons">
                      <div className="chatWindow--btn">
@@ -143,7 +131,7 @@ export default ({user}) => {
                 </div>
                 <div className="chatWindow--inputarea">
                     <input 
-                        className="chatWindow--input" type="search" placeholder="Digite uma mensagem" value={text} onChange={e => setText(e.target.value)}
+                        className="chatWindow--input" type="search" placeholder="Digite uma mensagem" value={text} onChange={e => setText(e.target.value)} onKeyUp={handleInputKeyUp}
                         />
                 </div>
                 <div className="chatWindow--pos">
